3889cc8fe700fbed804230713c6c451770287554
stajicvladimir <vstajic@gmail.com>
2018-07-27 15:58:22 +0200

    faking save u eklips

---
+++ WebContent/WEB-INF/view/helloworld-form.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+	<head>
+		<title>Hello World - Input Form</title>
+	</head>
+	
+	<form action="processForm" method="GET">
+		<input type="text" name="studentName" placeholder="What's your name?"/>
+		
+		<input type="submit"/>
+	</form>
+</html>
--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,5 +1,14 @@
 package com.luv2code.springdemo.mvc;
 
-public class HelloWorldController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
 }
