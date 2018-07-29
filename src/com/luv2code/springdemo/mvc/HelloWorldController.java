1aae68036e5080f409ab6121114874f469fd334d
vladimirstajic <vstajic@gmail.com>
2018-07-29 09:59:00 +0200

    first form submit done :)

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -9,6 +11,13 @@ public class HelloWorldController {
 	@RequestMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(Model model, 
+			@RequestParam("studentName") String studentName) {
+		model.addAttribute("studentName", studentName);
+		return "form-submit";
 	}
 	
 }
